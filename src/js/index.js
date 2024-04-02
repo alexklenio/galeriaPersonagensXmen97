6016836f70466ef43b11b52f2ffb350ff9c83b1c
alexklenio <alexlopes.see@gmail.com>
2024-04-02 18:46:59 -0300

    atualização

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-document.getElementById("ciclope");
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+  personagem.addEventListener("click", () => {
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagem.classList.add("selecionado");
+
+    const idPersonagem = personagem.id;
+
+    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.gif`;
+
+    const nomePersonagem = document.getElementById("nome-personagem");
+    nomePersonagem.innerText = personagem.getAttribute("data-name");
+
+    const descricaoPersonagem = document.getElementById("descricao-personagem");
+    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+  });
+});
